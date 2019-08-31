f65ace50b28d55f71ec5c5209ae8acf158b2c61e
Jethet <henriette.hettinga@gmail.com>
2019-08-31 19:13:29 +0200

    Create html page for create_hall function

--- hofvidz-project/hofvidz/urls.py
+++ hofvidz-project/hofvidz/urls.py
@@ -28,6 +28,6 @@ urlpatterns = [
     path('login', auth_views.LoginView.as_view(), name='login'),
     path('logout', auth_views.LogoutView.as_view(), name='logout'),
     # HALL
-    path('halloffame/create', views.create_hall, name='create_hall'),
+    path('halloffame/create', views.CreateHall.as_view(), name='create_hall'),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
