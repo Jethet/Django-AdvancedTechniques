5f738a53ead800e388f538fcee11808c728ffd8f
Jethet <henriette.hettinga@gmail.com>
2019-09-01 11:18:05 +0200

    Create view for creation hall, add combined login + create for new user

--- hofvidz-project/halls/views.py
+++ hofvidz-project/halls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 from .models import Hall
 
 def home(request):
@@ -11,6 +12,13 @@ class SignUp(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('home')
     template_name = 'registration/signup.html'
+    # This is meant to combine the user's signup with the creation of a hall:
+    def form_valid(self, form):
+        view = super(SignUp, self).form_valid(form)
+        username, password = form.cleaned_data.get('username'), form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+        return view
 
 
 class CreateHall(generic.CreateView):
